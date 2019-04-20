0737d9de7a08abaaa8e84b31fc54064472b974ad
Emily R <emilyroulund@gmail.com>
2019-04-20 02:32:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves (dwarf_names)
-  puts dwarf_names.each_with_index do |name, index + 1|
+  puts dwarf_names.each_with_index do |name, index|
 end
 
 def summon_captain_planet(planeteer_calls)
