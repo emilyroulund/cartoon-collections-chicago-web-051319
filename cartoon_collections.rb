c894d8dc4c4637981fa4c1771334ffd490cd26c5
Emily R <emilyroulund@gmail.com>
2019-04-20 02:34:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,7 @@ def roll_call_dwarves (dwarf_names)
 end
 
 def summon_captain_planet(planeteer_calls)
-  # capitalize each element and add an exclamation point at the end
-  # map or collect 
+ planeteer_calls.collect do |calls|.capitalize 
 end
 
 def long_planeteer_calls(planeteer_calls)
